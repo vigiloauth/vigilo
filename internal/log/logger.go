1a75fc9325f7d13d16b50fbbbcb7e39c3429db68
Olivier-Pimpare-Charbonneau <ocharb@mac.com>
2024-11-11 15:58:24 -0800

    Create internal logger directory

---
+++ internal/log/logger.go
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2024 Olivier Pimpare-Charbonneau, Zachary Sexton
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package log
+
+import (
+	"log"
+	"os"
+)
+
+var (
+	InfoLogger  *log.Logger
+	ErrorLogger *log.Logger
+	DebugLogger *log.Logger
+	WarnLogger  *log.Logger
+)
+
+func init() {
+	InfoLogger = log.New(os.Stdout, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
+	ErrorLogger = log.New(os.Stderr, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
+	DebugLogger = log.New(os.Stdout, "DEBUG: ", log.Ldate|log.Ltime|log.Lshortfile)
+	WarnLogger = log.New(os.Stdout, "WARN: ", log.Ldate|log.Ltime|log.Lshortfile)
+}
+
+func Info(message string) {
+	InfoLogger.Println(message)
+}
+
+func Error(message string) {
+	ErrorLogger.Println(message)
+}
+
+func Debug(message string) {
+	DebugLogger.Println(message)
+}
+
+func Warn(message string) {
+	WarnLogger.Println(message)
+}
