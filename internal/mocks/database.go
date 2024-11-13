87cdd0585577fd63f440c06125e6ee7246b8bb35
charb29 <ocharb@mac.com>
2024-11-12 23:15:02 -0800

    Update MockDatabase by adding default behavior.

--- internal/mocks/database.go
+++ internal/mocks/database.go
@@ -16,48 +16,145 @@
 
 package mocks
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 type MockDatabase struct {
+	mu sync.RWMutex
+
+	CreateFunc func(key string, value interface{}) error
+	ReadFunc   func(key string) (interface{}, error)
+	UpdateFunc func(key string, value interface{}) error
+	DeleteFunc func(key string) error
+
+	CreateCalls []struct {
+		Key   string
+		Value interface{}
+	}
+	ReadCalls []struct {
+		Key string
+	}
+	UpdateCalls []struct {
+		Key   string
+		Value interface{}
+	}
+	DeleteCalls []struct {
+		Key string
+	}
+
 	data map[string]interface{}
 }
 
 func NewMockDatabase() *MockDatabase {
-	return &MockDatabase{data: make(map[string]interface{})}
+	db := &MockDatabase{
+		data: make(map[string]interface{}),
+		CreateCalls: make([]struct {
+			Key   string
+			Value interface{}
+		}, 0),
+		ReadCalls: make([]struct {
+			Key string
+		}, 0),
+		UpdateCalls: make([]struct {
+			Key   string
+			Value interface{}
+		}, 0),
+		DeleteCalls: make([]struct {
+			Key string
+		}, 0),
+	}
+
+	db.CreateFunc = db.defaultCreate
+	db.ReadFunc = db.defaultRead
+	db.UpdateFunc = db.defaultUpdate
+	db.DeleteFunc = db.defaultDelete
+
+	return db
 }
 
-func (db *MockDatabase) Create(key string, value interface{}) error {
+func (db *MockDatabase) defaultCreate(key string, value interface{}) error {
+	db.mu.Lock()
+	defer db.mu.Unlock()
+
 	if _, exists := db.data[key]; exists {
 		return fmt.Errorf("create operation failed: record with key '%s' already exists", key)
 	}
-
 	db.data[key] = value
 	return nil
 }
 
-func (db *MockDatabase) Read(key string) (interface{}, error) {
+func (db *MockDatabase) defaultRead(key string) (interface{}, error) {
+	db.mu.RLock()
+	defer db.mu.RUnlock()
+
 	value, exists := db.data[key]
 	if !exists {
 		return nil, fmt.Errorf("read operation failed: no record found with key '%s'", key)
 	}
-
 	return value, nil
 }
 
-func (db *MockDatabase) Update(key string, value interface{}) error {
+func (db *MockDatabase) defaultUpdate(key string, value interface{}) error {
+	db.mu.Lock()
+	defer db.mu.Unlock()
+
 	if _, exists := db.data[key]; !exists {
 		return fmt.Errorf("update operation failed: no record found with key '%s'", key)
 	}
-
 	db.data[key] = value
 	return nil
 }
 
-func (db *MockDatabase) Delete(key string) error {
+func (db *MockDatabase) defaultDelete(key string) error {
+	db.mu.Lock()
+	defer db.mu.Unlock()
+
 	if _, exists := db.data[key]; !exists {
 		return fmt.Errorf("delete operation failed: no record found with key '%s'", key)
 	}
-
 	delete(db.data, key)
 	return nil
+}
+
+func (db *MockDatabase) Create(key string, value interface{}) error {
+	db.CreateCalls = append(db.CreateCalls, struct {
+		Key   string
+		Value interface{}
+	}{key, value})
+	return db.CreateFunc(key, value)
+}
+
+func (db *MockDatabase) Read(key string) (interface{}, error) {
+	db.ReadCalls = append(db.ReadCalls, struct {
+		Key string
+	}{key})
+	return db.ReadFunc(key)
+}
+
+func (db *MockDatabase) Update(key string, value interface{}) error {
+	db.UpdateCalls = append(db.UpdateCalls, struct {
+		Key   string
+		Value interface{}
+	}{key, value})
+	return db.UpdateFunc(key, value)
+}
+
+func (db *MockDatabase) Delete(key string) error {
+	db.DeleteCalls = append(db.DeleteCalls, struct {
+		Key string
+	}{key})
+	return db.DeleteFunc(key)
+}
+
+func (db *MockDatabase) Reset() {
+	db.mu.Lock()
+	defer db.mu.Unlock()
+
+	db.data = make(map[string]interface{})
+	db.CreateCalls = nil
+	db.ReadCalls = nil
+	db.UpdateCalls = nil
+	db.DeleteCalls = nil
 }
