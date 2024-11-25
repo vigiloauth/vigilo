1bdc0922a56e710e0c9014936f9f38d579de3cd7
Olivier-Pimpare-Charbonneau <ocharb@mac.com>
2024-11-24 21:05:22 -0800

    Add tests to validate password complexity.

--- internal/tests/user_handler_test.go
+++ internal/tests/user_handler_test.go
@@ -3,6 +3,7 @@ package tests
 import (
 	"bytes"
 	"encoding/json"
+	"github.com/vigiloauth/vigilo/identity/config"
 	"github.com/vigiloauth/vigilo/identity/server"
 	"github.com/vigiloauth/vigilo/internal/users"
 	"net/http"
@@ -13,10 +14,17 @@ import (
 const (
 	username string = "username"
 	email    string = "email@email.com"
-	password string = "password"
+	password string = "Pa$sword_123"
 )
 
 func TestUserHandler_HandleUserRegistration(t *testing.T) {
+	config.GetPasswordConfiguration().
+		SetRequireUppercase(true).
+		SetRequireNumber(true).
+		SetRequireSymbol(true).
+		SetMinimumLength(10).
+		Build()
+
 	tests := []struct {
 		name           string
 		requestBody    users.UserRegistrationRequest
@@ -38,25 +46,55 @@ func TestUserHandler_HandleUserRegistration(t *testing.T) {
 			requestBody: users.UserRegistrationRequest{
 				Username: "",
 				Email:    "invalidemail",
-				Password: "short",
-			},
-			expectedStatus: http.StatusBadRequest,
-			wantError:      true,
-		},
-		{
-			name: "Duplicate email during user registration",
-			requestBody: users.UserRegistrationRequest{
-				Username: username,
-				Email:    email,
 				Password: password,
 			},
-			expectedStatus: http.StatusConflict,
+			expectedStatus: http.StatusBadRequest,
 			wantError:      true,
 		},
 		{
 			name: "Missing required fields in request",
 			requestBody: users.UserRegistrationRequest{
 				Username: "missingemailuser",
+			},
+			expectedStatus: http.StatusBadRequest,
+			wantError:      true,
+		},
+		{
+			name: "Invalid password length",
+			requestBody: users.UserRegistrationRequest{
+				Username: username,
+				Email:    email,
+				Password: "password",
+			},
+			expectedStatus: http.StatusBadRequest,
+			wantError:      true,
+		},
+		{
+			name: "Password does not contains an uppercase letter",
+			requestBody: users.UserRegistrationRequest{
+				Username: username,
+				Email:    email,
+				Password: "password",
+			},
+			expectedStatus: http.StatusBadRequest,
+			wantError:      true,
+		},
+		{
+			name: "Password does not contain a number",
+			requestBody: users.UserRegistrationRequest{
+				Username: username,
+				Email:    email,
+				Password: "password",
+			},
+			expectedStatus: http.StatusBadRequest,
+			wantError:      true,
+		},
+		{
+			name: "Password does not contain a symbol",
+			requestBody: users.UserRegistrationRequest{
+				Username: username,
+				Email:    email,
+				Password: "password",
 			},
 			expectedStatus: http.StatusBadRequest,
 			wantError:      true,
@@ -70,10 +108,6 @@ func TestUserHandler_HandleUserRegistration(t *testing.T) {
 				t.Fatalf("failed to  marshal request body: %v", err)
 			}
 
-			if test.name == "Duplicate email during user registration" {
-				_ = users.GetUserCache().AddUser(users.User{Username: username, Password: password, Email: email})
-			}
-
 			rr := setupIdentityServer(body)
 			if rr.Code != test.expectedStatus {
 				t.Errorf("expected status %v, got %v", test.expectedStatus, rr.Code)
@@ -85,7 +119,7 @@ func TestUserHandler_HandleUserRegistration(t *testing.T) {
 					t.Fatalf("failed to unmarshal response body: %v", err)
 				}
 
-				if responseBody["error"] == nil {
+				if responseBody["error_code"] == nil {
 					t.Errorf("expected error in response, got none")
 				}
 			}
@@ -93,9 +127,28 @@ func TestUserHandler_HandleUserRegistration(t *testing.T) {
 	}
 }
 
+func TestUserHandler_DuplicateEmail(t *testing.T) {
+	requestBody := users.UserRegistrationRequest{
+		Username: username,
+		Email:    email,
+		Password: password,
+	}
+
+	_ = users.GetUserCache().AddUser(users.User{Username: username, Email: email})
+	body, err := json.Marshal(requestBody)
+	if err != nil {
+		t.Fatalf("failed to marshal request body: %v", err)
+	}
+
+	rr := setupIdentityServer(body)
+	if rr.Code != http.StatusConflict {
+		t.Errorf("expected status %v, got %v", http.StatusConflict, rr.Code)
+	}
+}
+
 func setupIdentityServer(body []byte) *httptest.ResponseRecorder {
 	vigiloIdentityServer := server.NewVigiloIdentityServer()
-	req := httptest.NewRequest(http.MethodPost, "/identity/users", bytes.NewBuffer(body))
+	req := httptest.NewRequest(http.MethodPost, "/vigilo/identity/users", bytes.NewBuffer(body))
 	rr := httptest.NewRecorder()
 	vigiloIdentityServer.Router.ServeHTTP(rr, req)
 
