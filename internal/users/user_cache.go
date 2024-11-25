49c6936a57c55b12d0190c110f5dcabe82410bed
Olivier-Pimpare-Charbonneau <ocharb@mac.com>
2024-11-24 17:55:28 -0800

    Create basic User cache.

---
+++ internal/users/user_cache.go
@@ -0,0 +1,42 @@
+package users
+
+import (
+	"sync"
+)
+
+type UserCache struct {
+	data map[string]User
+	mu   sync.RWMutex
+}
+
+var instance *UserCache
+var once sync.Once
+
+func GetUserCache() *UserCache {
+	once.Do(func() {
+		instance = &UserCache{
+			data: make(map[string]User),
+		}
+	})
+	return instance
+}
+
+func (c *UserCache) AddUser(user User) error {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	if _, ok := c.data[user.Email]; ok {
+		return &DuplicateUserError{Message: "user already exists with the given email"}
+	}
+
+	c.data[user.Email] = user
+	return nil
+}
+
+func (c *UserCache) GetUser(email string) (User, bool) {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+
+	user, found := c.data[email]
+	return user, found
+}
