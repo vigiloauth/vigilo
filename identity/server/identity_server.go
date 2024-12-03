89a7d6992ddf3849a3ff9dc4ff014b8cd6dfcae2
charb29 <ocharb@mac.com>
2024-12-02 16:40:16 -0800

    feat: update server to return routers.

--- identity/server/identity_server.go
+++ identity/server/identity_server.go
@@ -1,47 +1,38 @@
 package server
 
 import (
-	"fmt"
 	"github.com/go-chi/chi/v5"
 	"github.com/vigiloauth/vigilo/identity/handlers"
 	"github.com/vigiloauth/vigilo/internal/users"
-	"net/http"
 )
 
-const defaultPort = "8080"
-
-// VigiloIdentityServer represents the identity server responsible for handling HTTP requests related to user management.
+// VigiloIdentityServer represents the identity library's functionality.
 type VigiloIdentityServer struct {
-	Router      chi.Router
-	Port        string
+	router      chi.Router
 	userHandler *handlers.UserHandler
 }
 
-// NewVigiloIdentityServer creates and initializes a new instance of the VigiloIdentityServer.
-// It sets up the necessary routes and dependencies.
-func NewVigiloIdentityServer(port string) *VigiloIdentityServer {
+// NewVigiloIdentityServer creates and initializes a new instance of IdentityServer.
+// Automatically sets up routes.
+func NewVigiloIdentityServer() *VigiloIdentityServer {
 	userStore := users.GetInMemoryUserStore()
-	if port == "" {
-		port = defaultPort
-	}
+	userHandler := handlers.NewUserHandler(userStore)
+
 	server := &VigiloIdentityServer{
-		Router:      chi.NewRouter(),
-		Port:        port,
-		userHandler: handlers.NewUserHandler(userStore),
+		router:      chi.NewRouter(),
+		userHandler: userHandler,
 	}
-	server.setUpRoutes()
+
+	server.setupRoutes()
+
 	return server
 }
 
-// setUpRoutes sets up the HTTP routes for the identity server, mapping specific endpoints to handler functions.
-func (s *VigiloIdentityServer) setUpRoutes() {
-	s.Router.Post(users.UserEndpoints.Registration, s.userHandler.HandleUserRegistration)
+func (s *VigiloIdentityServer) setupRoutes() {
+	s.router.Post(users.UserEndpoints.Registration, s.userHandler.HandleUserRegistration)
 }
 
-// Start starts the Vigilo Identity Server, listening for incoming HTTP requests on the specified port.
-func (s *VigiloIdentityServer) Start() {
-	fmt.Printf("Starting Vigilo Identity Server on port %s\n", s.Port)
-	if err := http.ListenAndServe(":"+s.Port, s.Router); err != nil {
-		fmt.Printf("Error starting Vigilo Identity Server on port %s\n", s.Port)
-	}
+// Router returns the pre-configured router instance for integration.
+func (s *VigiloIdentityServer) Router() chi.Router {
+	return s.router
 }
