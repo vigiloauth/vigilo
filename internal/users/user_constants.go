d628318217bd634493d356ca32298f51f133490a
charb29 <ocharb@mac.com>
2025-02-26 16:43:29 -0800

    task: Add new constants related to login attempts.

--- internal/users/user_constants.go
+++ internal/users/user_constants.go
@@ -6,12 +6,20 @@ var TestConstants = struct {
 	Password        string
 	DuplicateEmail  string
 	InvalidPassword string
+	IPAddress       string
+	RequestMetadata string
+	Details         string
+	UserAgent       string
 }{
 	Username:        "username",
 	Email:           "vigilo@vigiloauth.com",
 	Password:        "Pa$s_w0rds",
 	DuplicateEmail:  "duplicate_email@test.com",
 	InvalidPassword: "invalid",
+	IPAddress:       "127.001.00",
+	RequestMetadata: "request_metadata",
+	Details:         "details",
+	UserAgent:       "user_agent",
 }
 
 var UserFieldConstants = struct {
