976670379d7fb9605e9f7915a57d7a0d34b8595e
charb29 <ocharb@mac.com>
2025-05-29 10:00:26 -0700

    style: create constants for timeouts.

--- internal/constants/timeouts.go
+++ internal/constants/timeouts.go
@@ -3,6 +3,8 @@ package constants
 import "time"
 
 const (
-	ThreeSecondTimeout time.Duration = 3 * time.Second
-	FiveSecondTimeout  time.Duration = 5 * time.Second
+	ThreeSecondTimeout  time.Duration = 3 * time.Second
+	FiveSecondTimeout   time.Duration = 5 * time.Second
+	ThirtySecondTimeout time.Duration = 30 * time.Second
+	TenSecondTimeout    time.Duration = 10 * time.Second
 )
