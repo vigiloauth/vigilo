2ac374f1057ed7b7a22777990b397e9cedd1e48a
charb29 <ocharb@mac.com>
2025-05-29 16:52:49 -0700

    task: setup jsx

--- ui/.eslintrc.cjs
+++ ui/.eslintrc.cjs
@@ -5,32 +5,16 @@ module.exports = {
   },
   extends: [
     'airbnb',
-    'airbnb-typescript',
     'airbnb/hooks',
     'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended'
+    'plugin:prettier/recommended',
   ],
-  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 'latest',
     sourceType: 'module',
-    project: './tsconfig.json',
   },
-  plugins: ['react', '@typescript-eslint'],
-  rules: {
-    'react/react-in-jsx-scope': 'off', 
-    'react/function-component-definition': [
-      2,
-      { namedComponents: 'arrow-function' },
-    ],
-  },
-  settings: {
-    react: {
-      version: '18.2',
-    },
-  },
+  rules: {},
 };
